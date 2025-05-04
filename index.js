7bb43c5cb58ecf322072720d3cfa332bfaafdae6
isha-web1 <isha.web1@gmail.com>
2025-05-04 23:04:10 +0600

    save user in db & get all user from db

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ async function run() {
     // database and collection
 
     const roomsCollection = client.db('roomX').collection('rooms')
+    const usersCollection = client.db('roomX').collection('users')
 
      // auth related api
 
@@ -92,6 +93,45 @@ async function run() {
           res.status(500).send(err)
         }
       })
+
+
+       // save a user data in db
+    app.put('/user', async (req, res) => {
+      const user = req.body
+      const query = { email: user?.email }
+      // check if user already exists in db
+      const isExist = await usersCollection.findOne(query)
+      if (isExist) {
+        if (user.status === 'Requested') {
+          // if existing user try to change his role
+          const result = await usersCollection.updateOne(query, {
+            $set: { status: user?.status },
+          })
+          return res.send(result)
+        } else {
+          // if existing user login again
+          return res.send(isExist)
+        }
+      }
+
+      // save user for the first time
+      const options = { upsert: true }
+      const updateDoc = {
+        $set: {
+          ...user,
+          timestamp: Date.now(),
+        },
+      }
+      const result = await usersCollection.updateOne(query, updateDoc, options)
+      res.send(result)
+    })
+
+
+        // get all users data from db
+        app.get('/users', async (req, res) => {
+          const result = await usersCollection.find().toArray()
+          res.send(result)
+        })
 
 
     // get all rooms from db
