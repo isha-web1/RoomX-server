f66819732b52b781e41d61e90ff25abf7e34f3f7
isha-web1 <isha.web1@gmail.com>
2025-05-09 20:22:44 +0600

    get all booking for a host & admin statistics route added

--- index.js
+++ index.js
@@ -299,16 +299,79 @@ async function run() {
         const result = await bookingsCollection.find(query).toArray()
         res.send(result)
       })
+
+
+        // get all booking for a host
+
+
+    app.get('/manage-bookings/:email',verifyToken, verifyHost,async (req, res) => {
+        const email = req.params.email
+        const query = { 'host.email': email }
+        const result = await bookingsCollection.find(query).toArray()
+        res.send(result)
+      }
+    )
   
 
          // delete a booking
 
-         
+
     app.delete('/booking/:id', verifyToken, async (req, res) => {
       const id = req.params.id
       const query = { _id: new ObjectId(id) }
       const result = await bookingsCollection.deleteOne(query)
       res.send(result)
+    })
+
+
+       // Admin Statistics
+
+       
+    app.get('/admin-stat', verifyToken, verifyAdmin, async (req, res) => {
+      const bookingDetails = await bookingsCollection
+        .find(
+          {},
+          {
+            projection: {
+              date: 1,
+              price: 1,
+            },
+          }
+        )
+        .toArray()
+
+      const totalUsers = await usersCollection.countDocuments()
+      const totalRooms = await roomsCollection.countDocuments()
+      const totalPrice = bookingDetails.reduce(
+        (sum, booking) => sum + booking.price,
+        0
+      )
+      // const data = [
+      //   ['Day', 'Sales'],
+      //   ['9/5', 1000],
+      //   ['10/2', 1170],
+      //   ['11/1', 660],
+      //   ['12/11', 1030],
+      // ]
+      const chartData = bookingDetails.map(booking => {
+        const day = new Date(booking.date).getDate()
+        const month = new Date(booking.date).getMonth() + 1
+        const data = [`${day}/${month}`, booking?.price]
+        return data
+      })
+      chartData.unshift(['Day', 'Sales'])
+      // chartData.splice(0, 0, ['Day', 'Sales'])
+
+      console.log(chartData)
+
+      console.log(bookingDetails)
+      res.send({
+        totalUsers,
+        totalRooms,
+        totalBookings: bookingDetails.length,
+        totalPrice,
+        chartData,
+      })
     })
 
 
